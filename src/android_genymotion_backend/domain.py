e8bfd1d19148118a0cf7404c3fd700437d11f62f
Vitalii Morskyi <vitalii@morskyi.dev>
2025-01-20 23:44:45 +0100

    Debugging what is wrong with domain counts

--- src/android_genymotion_backend/domain.py
+++ src/android_genymotion_backend/domain.py
@@ -381,6 +381,7 @@ class SessionModel(DynamoDBModel[Session]):
         for session in sessions_this_week_with_domain_name:
             domain_name = self.get_hosted_zone_name(session.domain_name)
             domain_counts[domain_name] = domain_counts.get(domain_name, 0) + 1
+        logger.info("Domain counts for the last week: %s", domain_counts)
         if domain_counts:
             least_used_domain_name = min(domain_counts, key=domain_counts.get)  # type: ignore
         else:
